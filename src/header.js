1a5106ac256fbae9024d0ba1c0b5090ba915c08c
Charles Clemens <cclemensjr@gmail.com>
2019-05-08 11:20:36 -0700

    Added render to Header

--- src/header.js
+++ src/header.js
@@ -4,6 +4,13 @@ class Header extends React.Component {
   constructor(props) {
     super(props);
   }
+
+  render() {
+    return (
+      <h1>{ this.props.title }</h1>
+      <p>{ this.props.prompt }</p>
+    );
+  }
 }
 
 export default Header;

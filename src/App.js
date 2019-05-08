7136032bafa0b8bca75ef03be0a3201fbbcd89a7
Charles Clemens <cclemensjr@gmail.com>
2019-05-08 12:06:22 -0700

    Rendered search in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './header.js';
 import Map from './map.js';
+import Search from './search';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,6 +13,7 @@ class App extends React.Component {
       <React.Fragment>
         <Header title="City Explorer" prompt="Enter a location below to learn about the weather, events, restaurants, movies filmed there, and more!" />
         <Map src="https://placeimg.com/640/480/any" alt="A placeholder image" query="Some random city over here!" />
+        <Search label="Search for a location" type="text" prompt="Explore!" />
       </React.Fragment>
     );
   }

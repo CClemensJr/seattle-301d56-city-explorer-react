14efb0fa406cbf6243e70c6837f1f949bb1ac1c6
Charles Clemens <cclemensjr@gmail.com>
2019-05-08 11:26:45 -0700

    Rendered header in app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ class App extends React.Component {
   constructor(props) {
     super(props);
   }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Header title="City Explorer" prompt="Enter a location below to learn about the weather, events, restaurants, movies filmed there, and more!" />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;

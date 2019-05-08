3296dac8978d57cde8fae108110f99fbcc3f3bdb
Charles Clemens <cclemensjr@gmail.com>
2019-05-08 12:03:25 -0700

    Added render method to search

--- src/search.js
+++ src/search.js
@@ -5,6 +5,21 @@ class Search extends React.Component {
     super(props);
   }
 
+  render() {
+    return (
+      <React.Fragment>
+        <form>
+          <label>
+            { this.props.label }
+
+            <input type={ this.props.type } /> 
+          </label>
+
+          <button>{ this.props.prompt }</button>
+        </form>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Search;

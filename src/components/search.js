a370e2a376f3822720c6802e5fedc0b7e5d94f83
Charles Clemens <cclemensjr@gmail.com>
2019-05-09 10:58:50 -0700

    Added submission handler

--- src/components/search.js
+++ src/components/search.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import superagent from 'superagent';
+import dotenv from 'dotenv';
 
 class SearchForm extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state ={  query: 'Seattle' };
   }
+
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    let apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${ this.query }&key=${ process.env.GOOGLE_MAPS_API_KEY}`;
+    let data = await superagent.get(apiUrl);
+    
+    this.props.handler(data);
+  };
 
   render() {
     return (
       <React.Fragment>
-        <form>
+        <form onSubmit={ this.handleSubmit }>
           <label>
             { this.props.label }
 
-            <input type={ this.props.type } /> 
+            <input type={ this.props.type } onChange={ this.handleChange }/> 
           </label>
 
           <button>{ this.props.prompt }</button>

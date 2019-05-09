b10151b1e7759f311fdc323755df31014f8cbed1
Charles Clemens <cclemensjr@gmail.com>
2019-05-09 10:13:26 -0700

    Updated search component and references to search form

--- src/search.js
+++ src/search.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class Search extends React.Component {
+class SearchForm extends React.Component {
   constructor(props) {
     super(props);
   }
@@ -22,4 +22,4 @@ class Search extends React.Component {
   }
 }
 
-export default Search;
+export default SearchForm;

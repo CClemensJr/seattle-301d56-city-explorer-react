8e4061ca96cec98d86ad1cb025c5939ad969e27c
Charles Clemens <cclemensjr@gmail.com>
2019-05-10 09:08:31 -0700

    updated Map

--- src/components/map.js
+++ src/components/map.js
@@ -1,32 +1,28 @@
 import React from 'react';
-import superagent from 'superagent';
-import SearchForm from './search.js'
-
 
 class Map extends React.Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      imgSrc: '',
-      queryPlaceHolder: 'Placeholder',
-      url: "https://city-explorer-backend.herokuapp.com/location"
-    };
-  }
+  //   this.state = {
+  //     imgSrc: '',
+  //     queryPlaceHolder: 'Placeholder',
+  //     url: "https://city-explorer-backend.herokuapp.com/location"
+  //   };
+  // }
 
-  handleForm = async submission => {
-    let location = await superagent.get(this.state.url).query(`data=${ submission }`);
+  // handleForm = async submission => {
+  //   let location = await superagent.get(this.state.url).query(`data=${ submission }`);
 
-    await this.setState({ imgSrc: `https://maps.googleapis.com/maps/api/staticmap?center=${location.body.latitude}%2c%20${location.body.longitude}&zoom=13&size=600x300&maptype=roadmap&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`});
-    await this.setState({ queryPlaceHolder: `Here are the results for ${location.body.formatted_query}`});
-  }
+  //   await this.setState({ imgSrc: `https://maps.googleapis.com/maps/api/staticmap?center=${location.body.latitude}%2c%20${location.body.longitude}&zoom=13&size=600x300&maptype=roadmap&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`});
+  //   await this.setState({ queryPlaceHolder: `Here are the results for ${location.body.formatted_query}`});
+  // }
 
   render() {
     return (
       <React.Fragment>
-        <SearchForm handler={ this.handleForm } />
-        <img id="map" src={ this.state.imgSrc } alt="Map of search query" />
-        <h2>{ this.state.queryPlaceHolder }</h2>
+        <img id="map" src={ this.props.src } alt="Map of search query" />
+        <h2>{ this.props.city_name }</h2>
       </React.Fragment>
     );
   }
